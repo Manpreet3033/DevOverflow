01fd1a769059ff78cf57f08eb5a6b3ed5acb86c9
Manpreet3033 <manpreet0855.be21@chitkara.edu.in>
2024-04-02 09:28:02 +0530

    Theme Switch Added

--- components/shared/navbar/Theme.tsx
+++ components/shared/navbar/Theme.tsx
@@ -17,7 +17,7 @@ const Theme = () => {
     <Menubar className="relative border-none bg-transparent shadow-none">
       <MenubarMenu>
         <MenubarTrigger className="focus:bg-light-900 data-[state=open]:bg-light-900 dark:focus:bg-dark-200 dark:data-[state=open]:bg-dark-200">
-          {mode === "" ? (
+          {mode === "light" ? (
             <Image
               className="active-theme cursor-pointer"
               width={20}
@@ -38,11 +38,10 @@ const Theme = () => {
         <MenubarContent className="absolute -right-12 mt-3 min-w-[120px] rounded border py-2 dark:border-dark-400 dark:bg-dark-300">
           {themes.map((item) => (
             <MenubarItem
-              className="flex cursor-pointer items-center gap-4 bg-light-900 px-2.5 py-2 focus:bg-light-800 dark:bg-dark-300 dark:focus:bg-dark-400"
+              className="flex items-center gap-4 px-2.5 py-2 dark:focus:bg-dark-200"
               key={item.value}
               onClick={() => {
                 setMode(item.value);
-
                 if (item.value !== "system") {
                   localStorage.theme = item.value;
                 } else {
--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -1,24 +1,30 @@
 "use client";
 
-import React, { createContext, useContext, useState, useEffect } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 
 interface ThemeContextType {
   mode: string;
   setMode: (mode: string) => void;
 }
 
-const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+export const ThemeContext = createContext<ThemeContextType | undefined>(
+  undefined
+);
 
-export function ThemeProvider({ children }: { children: React.ReactNode }) {
+const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
   const [mode, setMode] = useState("");
-
   const handleThemeChange = () => {
-    if (mode === "dark") {
-      setMode("light");
-      document.documentElement.classList.add("light");
-    } else {
+    const isDarkTheme =
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches);
+
+    if (isDarkTheme) {
       setMode("dark");
       document.documentElement.classList.add("dark");
+    } else {
+      setMode("light");
+      document.documentElement.classList.remove("dark");
     }
   };
 
@@ -31,12 +37,14 @@ export function ThemeProvider({ children }: { children: React.ReactNode }) {
       {children}
     </ThemeContext.Provider>
   );
-}
+};
 
-export function useTheme() {
+export default ThemeProvider;
+
+export const useTheme = () => {
   const context = useContext(ThemeContext);
   if (context === undefined) {
     throw new Error("useTheme must be used within a ThemeProvider");
   }
   return context;
-}
+};
