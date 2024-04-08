95bd86d8da33d259ed354dfe0670027d3b84fde0
Manpreet3033 <manpreet0855.be21@chitkara.edu.in>
2024-04-08 12:31:29 +0530

    Added Cached MongoDB Connection for faster connections to database

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,21 +1,38 @@
-import mongoose from "mongoose";
+import mongoose, { Mongoose } from "mongoose";
 
-let isConnected: boolean = false;
+const MONGO_DB_URL = process.env.MONGO_DB_URL;
+
+interface MongooseConnection {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
+}
+
+let cached: MongooseConnection = (global as any).mongoose;
+
+if (!cached) {
+  cached = (global as any).mongoose = {
+    conn: null,
+    promise: null,
+  };
+}
 
 export const connectToDatabase = async () => {
+  if (cached.conn) {
+    return cached.conn;
+  }
   mongoose.set("strictQuery", true);
-  if (!process.env.MONGO_DB_URL) {
+  if (!MONGO_DB_URL) {
     return console.log("Missing MongoDB url");
   }
-  if (isConnected) {
-    return console.log("MongoDB is already connected");
-  }
   try {
-    await mongoose.connect(process.env.MONGO_DB_URL, {
-      dbName: "devOverflow",
-    });
-    isConnected = true;
+    cached.promise =
+      cached.promise ||
+      mongoose.connect(MONGO_DB_URL, {
+        dbName: "devOverflow",
+      });
+    cached.conn = await cached.promise;
     console.log("Mongoose Connected");
+    return cached.conn;
   } catch (err) {
     console.log(err);
     throw err;
