d140d57dae7e5f9c1383dd9ebe1bf4fba12f0547
Manpreet3033 <manpreet0855.be21@chitkara.edu.in>
2024-04-08 10:36:08 +0530

    Added Tags Page and Fetch all tags server action

--- lib/actions/tag.actions.ts
+++ lib/actions/tag.actions.ts
@@ -1,8 +1,9 @@
 "use server";
 
+import Tag from "../database/models/tag.model";
 import User from "../database/models/user.model";
 import { connectToDatabase } from "../mongoose";
-import { GetTopInteractedTagsParams } from "./shared.types";
+import { GetAllTagsParams, GetTopInteractedTagsParams } from "./shared.types";
 
 export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
   try {
@@ -17,6 +18,18 @@ export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
       { _id: "2", name: "ExpressJS" },
       { _id: "3", name: "Javascript" },
     ];
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export async function getAllTags(params: GetAllTagsParams) {
+  try {
+    connectToDatabase();
+
+    const tags = await Tag.find({}).sort({ createdAt: -1 });
+
+    return { tags };
   } catch (err) {
     console.log(err);
   }
